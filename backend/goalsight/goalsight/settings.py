287bd27b73eb2a87fdca8a597d415a1f69188f42
Arina Zimina <111923358+Arino4kaMyr@users.noreply.github.com>
2025-07-03 00:01:52 +0300

    fix

--- backend/goalsight/goalsight/settings.py
+++ backend/goalsight/goalsight/settings.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
