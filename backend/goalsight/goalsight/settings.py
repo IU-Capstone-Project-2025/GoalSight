11f1713a07af835e344a9ad01b9209f6769e392a
Arina Zimina <111923358+Arino4kaMyr@users.noreply.github.com>
2025-07-14 21:26:11 +0300

    fix

--- backend/goalsight/goalsight/settings.py
+++ backend/goalsight/goalsight/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -42,7 +43,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "goalsight.urls"
@@ -153,7 +153,13 @@ SWAGGER_SETTINGS = {
     'DEFAULT_API_URL': 'http://localhost:8000',
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://goalsight.ru",
+    "https://www.goalsight.ru",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 ASGI_APPLICATION = "goalsight.asgi.application"
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
