bc9d047369e03e84d150e3e022b2db15d3d87625
Artem Panov <a.panov@innopolis.university>
2025-07-15 17:39:42 +0300

    fix of serializers.py

--- backend/goalsight/predictions/serializers.py
+++ backend/goalsight/predictions/serializers.py
@@ -3,3 +3,5 @@ from rest_framework import serializers
 class MatchPredictionSerializer(serializers.Serializer):
     home_win = serializers.FloatField(help_text="Probability of home team winning")
     away_win = serializers.FloatField(help_text="Probability of away team winning")
+    logo_url_64_home = serializers.URLField()
+    logo_url_64_away = serializers.URLField()
