bace9aabeaad7c316e5ad9dc72ed393884e198fe
Artem Panov <a.panov@innopolis.university>
2025-07-15 17:25:37 +0300

    Comments, code cleaning, adding structure, headings, comments to notebooks

--- backend/goalsight/predictions/serializers.py
+++ backend/goalsight/predictions/serializers.py
@@ -3,5 +3,3 @@ from rest_framework import serializers
 class MatchPredictionSerializer(serializers.Serializer):
     home_win = serializers.FloatField(help_text="Probability of home team winning")
     away_win = serializers.FloatField(help_text="Probability of away team winning")
-    logo_url_64_home = serializers.URLField() 
-    logo_url_64_away = serializers.URLField() 
