0dafb31e7b018f560c63eef9bec5ac150139e7fd
Arina Zimina <111923358+Arino4kaMyr@users.noreply.github.com>
2025-07-18 11:11:03 +0300

    update:match model

--- backend/goalsight/teams/models.py
+++ backend/goalsight/teams/models.py
@@ -8,16 +8,10 @@ class Team(models.Model):
     logo_url_32 = models.URLField(blank=True, null=True)
     logo_url_64 = models.URLField(blank=True, null=True)
     country = models.CharField(max_length=255)
-    coach = models.CharField(max_length=255)
     market_value = models.FloatField()
     avg_age = models.FloatField()
 
-    last_5_matches_wdl = models.JSONField()
-
     xG = models.FloatField()
-    ball_possession = models.FloatField()
-    shots_on_target = models.IntegerField()
-    big_chances_created = models.IntegerField()
 
     # Model features for ML predictions
     buildUpPlaySpeed = models.IntegerField(null=True, blank=True)
@@ -28,6 +22,22 @@ class Team(models.Model):
     defencePressure = models.IntegerField(null=True, blank=True)
     defenceAggression = models.IntegerField(null=True, blank=True)
     defenceTeamWidth = models.IntegerField(null=True, blank=True)
+
+    # === Custom team features for predictions ===
+    team_strength = models.FloatField(default=0.0)
+    league_strength = models.FloatField(default=0.0)
+    glicko2_rating = models.FloatField(default=1500.0)  
+    elo_rating = models.FloatField(default=1500.0)     
+
+    wins_last_5 = models.IntegerField(default=0)
+    losses_last_5 = models.IntegerField(default=0)
+    drawns_last_5 = models.IntegerField(default=0)
+    goal_avg_last_5 = models.FloatField(default=0.0)
+    avg_xG_last_5 = models.FloatField(default=0.0)
+    avg_xGA_last_5 = models.FloatField(default=0.0)
+
+    days_since_last_game = models.IntegerField(default=9999)  
+    matches_14_days = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
